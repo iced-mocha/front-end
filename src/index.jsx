a62fb488399e8f1d8a01af53714878daf2973d5f
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-10-05 08:11:24 -0700

    Create new project based of better example project

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import AppRoutes from './components/AppRoutes.jsx';
-
-window.onload = () => {
-    ReactDOM.render(<AppRoutes/>, document.getElementByID('main'))
-}
