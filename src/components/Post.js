28ed257ca360f5b5a4371aa5100ad46fa31567d0
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-10-21 18:58:25 -0700

    Add hero image

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,14 +3,23 @@ import { Link } from 'react-router-dom';
 import { ListGroupItem } from 'react-bootstrap';
 
 // To do use right icon for each kind of post
-export const Post = props => (
-	<ListGroupItem href="/">
-		<div className="post-container">
-			<img className="post-img what-the-frick" src={props.imgUrl} alt="Reddit icon" />
-			<div className="post-info">
-				<div className="post-title">{props.Title}</div>
-				<div className="post-date">{props.created}</div> <div className="post-author">Author: {props.Author}</div>
-			</div>
-		</div>
-	</ListGroupItem>
-);
+export const Post = props => {
+  return (
+    <ListGroupItem href="/">
+      <div className="post-container">
+      { props.HeroImg != "" && 
+        <div className="hero-img-container">
+          <img className="hero-img" src={props.HeroImg} />
+        </div>
+      }
+        <div className="post-description">
+          <img className="post-img what-the-frick" src={props.imgUrl} alt="Reddit icon" />
+          <div className="post-info">
+            <div className="post-title">{props.Title}</div>
+            <div className="post-date">{props.created}</div> <div className="post-author">Author: {props.Author}</div>
+          </div>
+        </div>
+      </div>
+    </ListGroupItem>
+  );
+}
