e8f11d849e26104acadedcd8e95acabc7c094d95
Jonathon Gore <jackgore@hotmail.ca>
2017-10-28 15:01:11 -0700

    added login and signup buttons

---
+++ src/components/login/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+import { Button }  from 'react-bootstrap';
+
+export class LoginButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  login(e) {
+	console.log("Test")
+  }
+
+  render() {
+    return (
+	  <button onClick={this.login}>Login</button>
+    );
+  }
+}
+
+export class SignupButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  signup(e) {
+
+	console.log("About to signup user")
+
+	// When this clicks we must show a text box and allow them to enter a user name
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'http://localhost:3000/v1/users', true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify({
+		    username: "userID"
+	}));
+  }
+
+  render() {
+    return (
+	  <button onClick={this.signup}>Signup</button>
+    );
+  }
+}
+
--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import FacebookSection from './FacebookSection'
 import RedditSection from './RedditSection'
+import { LoginButton, SignupButton } from './Login'
 
 class LoginPage extends React.Component {
   render() {
     return (
       <div className="login-page">
         <h1>Login</h1>
+		<LoginButton />
+		<SignupButton />
         <FacebookSection />
         <RedditSection />
       </div>
