cc0e4609d7d4209632daa5e8d8049f1085e1f885
Jonathon Gore <jackgore@hotmail.ca>
2017-11-12 22:45:52 -0800

    Why isnt settings page working

---
+++ src/components/settings/SettingsPage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import axios from 'axios';
+import { Jumbotron, FieldGroup, Button } from 'react-bootstrap';
+
+class SettingsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+	return (
+/*	  <Jumbotron>
+		  <form>
+			<FieldGroup
+			  id="formControlsText"
+			  type="text"
+			  label="Text"
+			  placeholder="Enter text"
+			/>
+			<FieldGroup
+			  id="formControlsEmail"
+			  type="email"
+			  label="Email address"
+			  placeholder="Enter email"
+			/>
+			<FieldGroup
+			  id="formControlsPassword"
+			  label="Password"
+			  type="password"
+			/>
+			<FieldGroup
+			  id="formControlsFile"
+			  type="file"
+			  label="File"
+			  help="Example block-level help text here."
+			/>
+
+			<Button type="submit">
+			  Submit
+			</Button>
+		  </form>
+	  </Jumbotron>	*/
+		<div>hello</div>
+	);
+  }
+}
+
+export default SettingsPage
