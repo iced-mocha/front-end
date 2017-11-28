7a0368ad3e88ba78dcf9daab9ff246d7d949a0f1
Jonathon Gore <jackgore@hotmail.ca>
2017-11-27 22:24:52 -0800

    Working sliders w/o save

---
+++ src/components/settings/Slider.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+export class WeightSlider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.imageForType = this.imageForType.bind(this);
+    this.state = {
+			title: props.title,
+			type: props.type,
+      value: props.value * 10,
+      onChange: props.onChange
+		};
+  }
+
+  imageForType(type) {
+		return "/img/" + type + "-icon.png";
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    this.state.onChange();
+  }
+
+  componentWillReceiveProps(props) {
+    this.state = {
+      type: props.type,
+      title: props.title,
+      value: props.value * 10,
+      onChange: props.onChange
+    };
+  }
+
+  // Note the max value is out of 100 - but we will actually use a range out of 10
+  // Putting the range to 100 allows for a smoother animation of the slider
+  render() {
+		return (
+      <div className='weight-slider-container'>
+          <img className="slider-img" src={this.imageForType(this.state.type)}/>
+          <div className='slider-value'>{Math.ceil(this.state.value / 10)}</div>
+          <input type="range" min="0" max="100" value={this.state.value}
+            className="weight-slider" id={this.state.type +"-slider"} onChange={this.handleChange}/>
+      </div>
+		);
+	}
+}
