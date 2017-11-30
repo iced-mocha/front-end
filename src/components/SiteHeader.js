d48cb0c7a10c1cf9c0bea1abfb664c1e451b3c61
Jonathon Gore <jackgore@hotmail.ca>
2017-11-29 23:23:22 -0800

    Updated styling for profile dropdown

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -7,23 +7,23 @@ import { LinkContainer } from 'react-router-bootstrap';
 class SignOutMenuItem extends React.Component {
   constructor(props) {
     super(props);
-	this.signout = this.signout.bind(this);
+	  this.signout = this.signout.bind(this);
   }
 
   signout() {
     axios({
-	  method: 'post',
-	  url: 'http://0.0.0.0:3000/v1/logout',
-	  withCredentials: true
-	});
-	// This function is passed into update state further up the tree
+	     method: 'post',
+	     url: 'http://0.0.0.0:3000/v1/logout',
+	     withCredentials: true
+	  });
+	  // This function is passed into update state further up the tree
     this.props.stateUpdate();
   }
 
   render() {
-	return (
+	   return (
         <MenuItem onSelect={this.signout}>Logout</MenuItem>
-	);
+	   );
   }
 
 }
@@ -32,82 +32,81 @@ class LoggedInAsMenuItem extends React.Component {
     super(props);
     this.state = { username: props.username }
   }
-  
+
   componentWillReceiveProps(nextProps) {
-	this.setState({ username: nextProps.username });  
+	   this.setState({ username: nextProps.username });
   }
 
   render() {
     if (this.state.username === undefined) {
 		this.state.username = "";
     }
-	
-    return (<MenuItem header className='menu-header'>Signed in as <br/><div className='menu-header menu-header-value'>{this.state.username}</div></MenuItem>);
+
+    return (<MenuItem header className='menu-header'><div className='signed-in-title'>Signed in as </div><div className='menu-header signed-in-value'>{this.state.username}</div></MenuItem>);
   }
 }
 
 class AccountAction extends React.Component {
   constructor(props) {
     super(props);
-	this.signoutStateUpdate = this.signoutStateUpdate.bind(this);
-	this.state = { user: {}};
+    this.signoutStateUpdate = this.signoutStateUpdate.bind(this);
+    this.state = { user: {}};
   }
-  
+
   componentWillReceiveProps(nextProps) {
-	this.setState({ loggedIn: nextProps.loggedIn, user: nextProps.user });  
+    this.setState({ loggedIn: nextProps.loggedIn, user: nextProps.user });
   }
 
   signoutStateUpdate() {
-	this.setState({ loggedIn: false})
+    this.setState({ loggedIn: false})
   }
 
-  render() { 
-	if (this.state.loggedIn == undefined) {
-	  return <NavItem href="/login"></NavItem>
-	} else if (!this.state.loggedIn) {
-	  return <NavItem href="/login">Login</NavItem>
+  render() {
+    if (this.state.loggedIn == undefined) {
+      return <NavItem href="/login"></NavItem>
+    } else if (!this.state.loggedIn) {
+      return <NavItem href="/login">Login</NavItem>
     }
-	return(
-        <NavDropdown eventKey="4" title="Profile" id="nav-dropdown">
-		  <LoggedInAsMenuItem username={this.state.user['username']}/>
+
+   return(
+     <NavDropdown eventKey="4" title="Profile" id="nav-dropdown">
+        <LoggedInAsMenuItem username={this.state.user['username']}/>
           <MenuItem divider />
-		  <LinkContainer to="/settings">
-            <MenuItem eventKey="4.1">Settings</MenuItem>
-		  </LinkContainer>
+        <LinkContainer to="/settings">
+          <MenuItem eventKey="4.1">Settings</MenuItem>
+        </LinkContainer>
           <MenuItem divider />
-	      <SignOutMenuItem stateUpdate={this.signoutStateUpdate}/>
-        </NavDropdown>
-	)
+        <SignOutMenuItem stateUpdate={this.signoutStateUpdate}/>
+      </NavDropdown>
+    );
   }
 }
 
 class SiteHeader extends React.Component {
   constructor(props) {
     super(props);
-	this.state = { loggedIn: props.loggedIn, user: props.user }
+	  this.state = { loggedIn: props.loggedIn, user: props.user }
   }
-  
+
   componentWillReceiveProps(nextProps) {
-	this.setState({ loggedIn: nextProps.loggedIn, user: nextProps.user });  
+	  this.setState({ loggedIn: nextProps.loggedIn, user: nextProps.user });
   }
 
   render() {
     return (
-		<Navbar>
+	    <Navbar id='nav' className='site-font'>
+		    <Navbar.Header>
+			    <Navbar.Brand>
+			      <a href="/">Iced-Mocha</a>
+          </Navbar.Brand>
+        </Navbar.Header>
 
-			  <Navbar.Header>
-				<Navbar.Brand>
-				  <a href="/">Iced-Mocha</a>
-				</Navbar.Brand>
-			  </Navbar.Header>
-
-			  <Navbar.Collapse>
-				<Nav pullRight>
-				  <AccountAction loggedIn={this.state.loggedIn} user={this.state.user}/>
-				</Nav>
-			  </Navbar.Collapse>
-
-		</Navbar>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <AccountAction loggedIn={this.state.loggedIn} user={this.state.user}/>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
