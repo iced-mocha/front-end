1fd6dcff199007898becdf13d6092ec19ae9c4c8
Jonathon Gore <jackgore@hotmail.ca>
2017-12-16 14:16:44 -0800

    allowed for twitter images ang videos

--- src/components/posts/Tweet.js
+++ src/components/posts/Tweet.js
@@ -7,18 +7,20 @@ class Tweet extends React.Component {
     super(props, context);
     this.parseTweet = this.parseTweet.bind(this);
     this.getDateMessage = this.getDateMessage.bind(this);
-    var metaObj = JSON.parse(this.props.meta);
+    var metaObj = JSON.parse(this.props.meta)['extended_entities'];
     this.state = {
-      hashtags: metaObj.entities.hashtags ? metaObj.entities.hashtags : [],
-      mentions: metaObj.entities['user_mentions'] ? metaObj.entities['user_mentions'] : [],
-      urls: metaObj.entities.urls ? metaObj.entities.urls : []
+      hashtags: metaObj.hashtags ? metaObj.hashtags : [],
+      mentions: metaObj['user_mentions'] ? metaObj['user_mentions'] : [],
+      urls: metaObj.urls ? metaObj.urls : [],
+      media: metaObj.media ? metaObj.media : [],
+      displayMedia: []
     };
   }
 
   parseTweet(text) {
     // TODO - probably shouldnt block the rest of operations to do This
     // i.e put plain non parsed text in first - then replace everything
-
+    console.log(text);
     // Replace hashtags with links
     for (var i = 0; i < this.state.hashtags.length; i++) {
       var re = new RegExp(this.state.hashtags[i]['text_with_hash'],'g');
@@ -27,13 +29,43 @@ class Tweet extends React.Component {
     }
 
     for (var i = 0; i < this.state.mentions.length; i++) {
-      console.log(this.state.mentions[i]);
       var re = new RegExp('@' + this.state.mentions[i]['screen_name'],'g');
       text = text.replace(re, "<a class='twitter-author-link' href='" + this.state.mentions[i].link + "' target='blank'> @" +
         this.state.mentions[i]['screen_name'] + '</a>');
     }
 
+    for (var i = 0; i < this.state.media.length; i++) {
+      this.buildMedia(i, this.state.media[i]);
+      var re = new RegExp(this.state.media[i].url,'g');
+      text = text.replace(re, "");
+    }
+
     return text;
+  }
+
+  buildMedia(id, media) {
+    console.log(media);
+    // PUT this in state 'photos'
+    if (media.type === 'photo') {
+      this.state.displayMedia.push(
+        <div key={id} className="hero-img-container">
+          <img className="hero-img" src={media['media_url_https']} />
+        </div>
+      )
+      this.setState({ displayMedia: this.state.displayMedia});
+    } else if (media.type === 'video') {
+      var videoInfo = media['video_info']['variants']
+      for (var i = 0; i < videoInfo.length; i++) {
+        if(videoInfo[i]['content_type'] === 'video/mp4') {
+          this.state.displayMedia.push(
+             <Video src={videoInfo[i].url} autoPlay={true}
+              loop={true} muted={true}/>
+          )
+          this.setState({ displayMedia: this.state.displayMedia});
+          return;
+        }
+      }
+    }
   }
 
   getDateMessage(date) {
@@ -62,6 +94,12 @@ class Tweet extends React.Component {
     return dateMessage
   }
 
+  componentWillMount() {
+    this.setState({
+        content: this.parseTweet(this.props.text)
+    });
+  }
+
   render() {
     return (
       <div className='tweet-container'>
@@ -73,7 +111,10 @@ class Tweet extends React.Component {
             </a>
             <span className='tweet-time'>{'tweeted ' + this.getDateMessage(new Date(this.props.date))}</span>
           </span>
-          <div className='tweet-text' dangerouslySetInnerHTML={{ __html: this.parseTweet(this.props.text)}}/>
+          <div className='tweet-text' dangerouslySetInnerHTML={{ __html: this.state.content}}/>
+          <div className='tweet-media-container'>
+            {this.state.displayMedia}
+          </div>
           <div className='tweet-stats'>
             <span className='tweet-rts'>
               <FontAwesome className='retweet-icon' name='retweet' />
